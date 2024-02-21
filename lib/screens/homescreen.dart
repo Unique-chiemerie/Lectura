c25f85be9ebf292d0f6a80b426e3d01733fd5859
Unique-chiemerie <lilanimate1@gmail.com>
2024-02-21 08:43:27 +0100

    styling the FAB

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,16 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class Homescreen extends StatelessWidget {
+class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
 
   @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: const Center(
         child: Text('Work in progress'),
       ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.transparent,
         onPressed: () {},
+        child: SvgPicture.asset(
+          'assets/AddFAB.svg',
+          height: screenHeight * 0.1,
+          width: screenWidth * 0.05,
+        ),
       ),
     );
   }
