302642fbd6ad34785788176ba349bde815b5ce18
Unique-chiemerie <lilanimate1@gmail.com>
2024-02-28 10:28:30 +0100

    adjusting the small icon

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -10,10 +10,24 @@ class Settingscreen extends StatefulWidget {
 class _SettingscreenState extends State<Settingscreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Work in progress'),
-      ),
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    return SafeArea(
+      child: Scaffold(
+          body: Column(
+        children: [
+          //little spacce before the icon
+          SizedBox(
+            height: screenHeight * 0.1,
+          ),
+          //the avatar
+          SizedBox(
+            height: screenHeight * 0.2,
+            width: screenWidth * 0.3,
+            child: const Placeholder(),
+          ),
+        ],
+      )),
     );
   }
 }
