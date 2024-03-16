e2d7c874221f8f4f61f206ee3f2832ea96ffac8d
Unique-chiemerie <lilanimate1@gmail.com>
2024-03-16 15:51:05 +0100

    setting up a state management for the user info/data

---
+++ lib/data/userdata.dart
@@ -0,0 +1,7 @@
+//create some sort of user class for effective state management
+class UserData {
+  final String name;
+  final String dept;
+  final String level;
+  UserData({required this.name, required this.dept, required this.level});
+}
