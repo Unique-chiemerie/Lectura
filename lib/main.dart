d4731f599abd3d5fcc9cdf535bde7a22ac543b22
Unique-chiemerie <lilanimate1@gmail.com>
2024-03-19 02:26:00 +0100

    setting up provider properly

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:lecture_app/data/userdata.dart';
 import 'package:lecture_app/splashscreen/lectura_splashscreen.dart';
 import 'package:lecture_app/theme/lectura_theme.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   //initialising hive
@@ -13,11 +15,14 @@ void main() async {
   var course = await Hive.openBox('course');
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: lecturaTheme,
-      title: 'Lectura',
-      home: const Lecturasplash(),
+    Provider<UserDataProvider>(
+      create: (context) => UserDataProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: lecturaTheme,
+        title: 'Lectura',
+        home: const Lecturasplash(),
+      ),
     ),
   );
 }
